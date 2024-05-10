0c468f23bd6964eaab7cccbf3716555949719a84
Faith <loshyfaith@gmail.com>
2024-05-10 14:43:08 +0300

    fetch crud lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,93 @@
-import React, { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
   const [page, setPage] = useState("List");
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  const fetchQuestions = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/questions");
+      if (!response.ok) {
+        throw new Error("Failed to fetch questions");
+      }
+      const data = await response.json();
+      setQuestions(data);
+    } catch (error) {
+      console.error("Error fetching questions:", error);
+    }
+  };
+
+  const handleNewQuestionSubmit = async (formData) => {
+    try {
+      const response = await fetch("http://localhost:4000/questions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to create question");
+      }
+      fetchQuestions();
+      setPage("List"); // Switch back to question list page after adding a new question
+    } catch (error) {
+      console.error("Error creating question:", error);
+    }
+  };
+
+  const handleDeleteQuestion = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:4000/questions/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete question");
+      }
+      setQuestions(questions.filter((question) => question.id !== id));
+    } catch (error) {
+      console.error("Error deleting question:", error);
+    }
+  };
+
+  const handleUpdateCorrectAnswer = async (id, correctIndex) => {
+    try {
+      const response = await fetch(`http://localhost:4000/questions/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ correctIndex }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to update correct answer");
+      }
+      fetchQuestions();
+    } catch (error) {
+      console.error("Error updating correct answer:", error);
+    }
+  };
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onSubmit={handleNewQuestionSubmit} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateCorrectAnswer={handleUpdateCorrectAnswer}
+        />
+      )}
     </main>
   );
 }
